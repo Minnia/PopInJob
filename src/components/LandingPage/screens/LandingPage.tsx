9ab400e3a3ef4ad10ef80d1b3d75f86a2e388969
Minnia <jasminestockholm@gmail.com>
2020-02-24 19:57:06 +0100

    fix folder structure for LandingPage

--- src/components/LandingPage/screens/LandingPage.tsx
+++ src/components/LandingPage/screens/LandingPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Background from "../LandingPageBackground";
-import NavBar from "../reusableComponents/Navbar";
+import NavBar from "../../reusableComponents/Navbar";
 import ContentContainer from "../ContentContainer";
 import CollaboratingCompanies from "../CollaboratingCompanies";
 import NewsLetterBox from "../NewsLetterBox";
