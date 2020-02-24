12ad535fcd88baaa54e56224d92c0426fd575403
Minnia <jasminestockholm@gmail.com>
2020-02-24 18:58:06 +0100

    add NewsLetterText and more styling to form

--- src/components/NewsLetterBox/NewsLetterBox.tsx
+++ src/components/NewsLetterBox/NewsLetterBox.tsx
@@ -1,17 +1,36 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import { NewsLetterFormSignup, NewsLetterFormInput } from "./styled";
+import {
+  NewsLetterFormSignup,
+  NewsLetterFormInput,
+  NewsLetterText
+} from "./styled";
 
 const NewsLetterBox = () => {
   return (
-    <NewsLetterFormSignup>
-      <NewsLetterFormInput placeholder="First name" />
-      <NewsLetterFormInput placeholder="Last name" />
-      <NewsLetterFormInput placeholder="Email" />
-      <Button className="newsletterButton" variant="outline-dark">
-        Sign up
-      </Button>
-    </NewsLetterFormSignup>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center"
+      }}
+    >
+      <NewsLetterText>
+        <h1>Want to learn more?</h1>
+        <p>
+          Sign up with your email address to receive news and updates about open
+          job positions
+        </p>
+      </NewsLetterText>
+      <NewsLetterFormSignup>
+        <NewsLetterFormInput placeholder="First name" />
+        <NewsLetterFormInput placeholder="Last name" />
+        <NewsLetterFormInput placeholder="Email" />
+        <Button className="newsletterButton" variant="outline-dark">
+          Sign up
+        </Button>
+      </NewsLetterFormSignup>
+    </div>
   );
 };
 
--- src/components/NewsLetterBox/styled.ts
+++ src/components/NewsLetterBox/styled.ts
@@ -3,17 +3,18 @@ import constants from "../../constants";
 
 export const NewsLetterFormSignup = styled.form`
   width: 100vw;
-  height: 300px;
+  height: 200px;
   display: flex;
   flex-direction: row;
   justify-content: center;
-  align-items: center;
+  align-items: baseline;
   background-color: ${constants.colors.landingPagePurple};
 `;
 
 export const NewsLetterFormInput = styled.input`
   width: 200px;
   height: 50px;
+  margin-top: 50px;
   margin-right: 16px;
   outline: none;
   border-style: none;
@@ -37,4 +38,14 @@ export const NewsLetterFormInput = styled.input`
     color: gray;
     padding-left: 16px;
   }
+`;
+
+export const NewsLetterText = styled.div`
+  display: flex;
+  height: 200px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  color: white;
+  background: ${constants.colors.landingPagePurple};
 `;
