070a2cbf6ecae0262ad696ded159c6d50fad7749
Venomaleks <aleks.edholm@gmail.com>
2020-03-04 13:47:54 +0100

    feature/logosImage

--- src/components/LandingPage/CollaboratingCompanies/CollaboratingCompanies.tsx
+++ src/components/LandingPage/CollaboratingCompanies/CollaboratingCompanies.tsx
@@ -1,14 +1,64 @@
 import React from "react";
-import { CollaboratorsDiv } from "./styled";
+import Image from "react-bootstrap/Image";
+import { 
+  CollaboratorsDiv,
+  LogTitel,
+  LogoSize
+  
+} from "./styled";
 
 const CollaboratingCompanies = () => {
+  const T2 = require("../../../../src/assets/images/T2 tea.png");
+  const Unilever = require("../../../../src/assets/images/Unilever.jpeg");
+  const PepsiCo = require("../../../../src/assets/images/PepsiCo.png");
+  const Gc = require("../../../../src/assets/images/Gc.png");
+  const LV = require("../../../../src/assets/images/LV.png");
+  const DIOR = require("../../../../src/assets/images/DIOR.jpeg");
+  const Harrods = require("../../../../src/assets/images/Harrods.jpeg");
+  const HM = require("../../../../src/assets/images/H&M.png");
+  const Coca = require("../../../../src/assets/images/CocaCola.png");
+
   return (
-    <CollaboratorsDiv>
-      <h1>Logo 1</h1>
-      <h1>Logo 2</h1>
-      <h1>Logo 3</h1>
-      <h1>Logo 4</h1>
-    </CollaboratorsDiv>
+    <div>
+
+      <div>
+        <LogTitel>SOME OF OUR VALUED BRANDS</LogTitel>
+      </div>
+      <LogoSize>
+      <CollaboratorsDiv>
+        
+          <Image src={T2} fluid />
+        
+          <Image src={Unilever} fluid />
+        
+          <Image src={PepsiCo} fluid />
+         
+          <Image src={Gc} fluid />
+
+      </CollaboratorsDiv>
+      </LogoSize>
+
+      <hr/>
+
+      <div>
+        <LogTitel>SOME OF OUR COLLY's HAVE WORKED FOR</LogTitel>
+      </div>
+
+      <CollaboratorsDiv>
+        
+        <Image src={LV} fluid />
+        
+        <Image src={DIOR} fluid />
+        
+        <Image src={HM} fluid />
+                
+        <Image src={Harrods} fluid />
+        
+        <Image src={Coca} fluid />
+
+      </CollaboratorsDiv>
+
+    </div>
   );
 };
 
