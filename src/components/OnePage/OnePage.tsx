71ac023dc17d412c9ca9f964288eb73c77d3f281
Minnia <jasminestockholm@gmail.com>
2020-03-10 11:53:47 +0100

    add initial OnePage component and screen

---
+++ src/components/OnePage/OnePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../reusableComponents/Navbar";
+
+const OnePage = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default OnePage;
---
+++ src/components/OnePage/screens/OnePageScreen.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OnePageScreen = () => {
+  return <div></div>;
+};
+
+export default OnePageScreen;
