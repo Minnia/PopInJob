42489d15884a5be7f26b8854948f8fc8d75bca63
Venomaleks <aleks.edholm@gmail.com>
2020-04-06 13:29:20 +0200

    top-bottom scroll

--- src/screens/LandingScreen/LandingScreen.tsx
+++ src/screens/LandingScreen/LandingScreen.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import Background from "../../components/LandingPage/LandingPageBackground";
 import NavBar from "../../components/reusableComponents/Navbar";
 import ContentContainer from "../../components/reusableComponents/ContentContainer";
@@ -8,11 +9,43 @@ import UserStatistics from "../../components/LandingPage/UserStatistics";
 import Footer from "../../components/reusableComponents/Footer";
 import SigninComponet from "../../components/LandingPage/SignupComponent";
 
+import AboutUsContainer from "../../components/AboutPage/AboutUsContainer";
+import YourJourney from "../../components/YouPage/YourJourney";
+
+import Scroll from 'react-scroll';
+import {Link, Element} from 'react-scroll'
+
+var scroll = Scroll.animateScroll;
+
+scroll.scrollToTop();
+
+scroll.scrollToBottom();
+
+
+
+//scroll Element = Scroll.Element;
+
 const LandingPage = () => {
+
   return (
     <Background>
       <NavBar />
+      
+      <Link onClick={() => scroll.scrollToBottom()}>Scroll To Bottom</Link>
+
+      <br/>
+
+      <Link onClick={() => scroll.scrollTo(1000)}>Scroll To About</Link>
+      
+
       <SigninComponet />
+      
+      <hr/>
+      
+      <YourJourney />
+      
+      <hr/>
+      
       <ContentContainer
         contentText={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id
           ultricies metus, nec ornare dui. Nullam accumsan risus sapien, sed
@@ -37,6 +70,13 @@ const LandingPage = () => {
       <CollaboratingCompanies />
       <NewsLetterBox />
       <UserStatistics />
+     
+      <hr/>
+     
+      <AboutUsContainer/>
+
+      <Link onClick={() => scroll.scrollToTop()}>Scroll To Top</Link>
+      
       <Footer />
     </Background>
   );
