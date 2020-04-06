f604b0061abad45dfe90396a5ba5b75d8fc1d16d
Venomaleks <aleks.edholm@gmail.com>
2020-04-06 13:55:19 +0200

    word Fix

--- src/components/SignupPage/SignupCustomer/SignupCustomer.tsx
+++ src/components/SignupPage/SignupCustomer/SignupCustomer.tsx
@@ -6,7 +6,7 @@ import {
   SignupButton,
   ComponentContaner,
   RowObjects,
-  Signdiv,
+  SignDiv,
   CheckBox
   
   } from "./styled";
@@ -24,7 +24,7 @@ const SignupCustomer = () => {
     <ComponentContaner>
       <Tital>Signup page</Tital>
     
-      <Signdiv>
+      <SignDiv>
           
         <Text>Your email</Text>      
         <InputFeiled/>
@@ -54,7 +54,7 @@ const SignupCustomer = () => {
 
         <SignupButton> Sign in </SignupButton>
     
-      </Signdiv>
+      </SignDiv>
     </ComponentContaner>
   </>
   );
